3015f069186cde6d74e41afe7ffca744968c22b9
solidogen <solidogen@gmail.com>
2022-04-10 23:20:08 +0200

    1:25:00, ts props, cms query, post responsive grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Header from '../components/Header'
 import TopBanner from '../components/TopBanner'
+import { sanityClient, urlFor } from '../lib/sanity'
+import { Post } from '../model/typings'
 
-export default function Home() {
+interface HomeProps {
+  posts: [Post]
+}
+
+export default function Home(props: HomeProps) {
+  const posts = props.posts
+  console.log(posts)
   return (
-    <div className="max-w-7xl mx-auto">
+    <div className="mx-auto max-w-7xl">
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
@@ -13,6 +22,49 @@ export default function Home() {
       <Header />
 
       <TopBanner />
+
+      {/* Posts */}
+      <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div>
+              <img src={urlFor(post.mainImage).url()!} alt="" />
+              <div className="flex justify-between bg-white p-5">
+                <div>
+                  <p>{post.title}</p>
+                  <p>
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const postsQuery = `*[_type == "post"]{
+    _id,
+    title,
+    slug,
+    author -> {
+      name, 
+      image
+    },
+    description,
+    mainImage,
+  }`
+  const posts = await sanityClient.fetch(postsQuery)
+  return {
+    props: { posts },
+  }
 }
