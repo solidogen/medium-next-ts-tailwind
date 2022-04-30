8397824caeefbdd2825100341d2228a346ca2ed9
solidogen <solidogen@gmail.com>
2022-04-30 13:45:26 +0200

    1:57:00, post details first UI part

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from 'next'
 import Header from '../../components/Header'
-import { sanityClient } from '../../lib/sanity'
+import { sanityClient, urlFor } from '../../lib/sanity'
 import { Post } from '../../model/typings'
 
 interface Props {
@@ -9,10 +9,35 @@ interface Props {
 
 function Post(props: Props) {
   let post = props.post
-  console.log(post)
   return (
     <main>
       <Header />
+
+      <img
+        className="h-40 w-full object-cover"
+        src={urlFor(post.mainImage).url()!}
+        alt=""
+      />
+
+      <article className="mx-auto max-w-3xl p-5">
+        <h1 className="mt-10 mb-3 text-3xl">{post.title}</h1>
+        <h2 className="mb-2 text-xl font-light text-gray-500">
+          {post.description}
+        </h2>
+
+        <div className="flex items-center space-x-2 border border-red-500">
+          <img
+            className="h-10 w-10 rounded-full"
+            src={urlFor(post.author.image).url()!}
+            alt=""
+          />
+          <p className="text-sm font-extralight">
+            Blog post by{' '}
+            <span className="text-green-600">{post.author.name}</span> -
+            Published at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+      </article>
     </main>
   )
 }
@@ -75,6 +100,6 @@ export const getStaticProps: GetStaticProps = async (context) => {
     },
     // enables ISR - after user loads the page, next loads will be from cache,
     // after this time next user will wait for a fresh page
-    revalidate: 60
+    revalidate: 60,
   }
 }
